2e5fd90d8133a810de718cbeede01dd89bf13f37
izzyfro21 <isaacncolon@rocketmail.com>
2019-04-07 16:54:49 -0400

    Update spambot.py

--- spambot.py
+++ spambot.py
@@ -1,23 +1,22 @@
-
 import smtplib
 import time
 import sys
 import os
 
 
-
+#Asks the user for all the necessary inputs to make this program run
 print("Before you start, you must go to this website: https://myaccount.google.com/lesssecureapps and check the box yes")
+email = str(raw_input("What is your email (must be gmail): "))
+password = raw_input("What is your password: ")
+amount = int(raw_input("How many emails would you like to send: "))
+wait = int(raw_input("How long before each message (in seconds): "))
+recipient = raw_input("What is the recipiant email: ")
 
-email = input("What is your email (must be gmail): ")
-password = input("What is your password: ")
-amount = int(input("How many emails would you like to send: "))
-wait = int(input("How long before each message (in seconds): "))
-recipient = input("What is the recipiant email: ")
-
+#creats the text editor
 print("Type in your message.")
 lines = []
 while True:
-    line = input(">")
+    line = raw_input(">")
     if line:
         lines.append(line)
     else:
@@ -26,6 +25,7 @@ text = '\n'.join(lines)
 
 starttime = time.time()
 
+#sends the email
 for i in range(amount):
     smtpObj = smtplib.SMTP('smtp.gmail.com', 587)
 
@@ -38,7 +38,5 @@ for i in range(amount):
     smtpObj.quit()
 
     time.sleep(wait)
-
-
 
 
